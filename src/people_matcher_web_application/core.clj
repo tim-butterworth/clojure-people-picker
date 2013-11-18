5cbe363c3930301235f37f5d3b3c8a4119347e05
Tim Butterworth <tdbmanta@gmail.com>
2013-11-18 07:20:30 -0500

    fixed the port issue hopefully

--- src/people_matcher_web_application/core.clj
+++ src/people_matcher_web_application/core.clj
@@ -12,7 +12,15 @@
 (def app
   (-> handler wrap-params))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (run-jetty handler {:port 3000}))
+(defn start [port]
+  (run-jetty handler {:port port :join? false}))
+
+(defn -main []
+  (let [port (Integer/parseInt
+               (or (System/getenv "PORT") "3000"))]
+  (start port)))
+
+;(defn -main
+;  "I don't do a whole lot ... yet."
+;  [& args]
+;  (run-jetty handler {:port 3000}))
